49ba664c4fa8ea708b7f98e926c4e9eb8e09cb7e
patinkaew <pinkaew@dx1-915-hep.hip.helsinki.fi>
2023-03-08 16:40:45 +0200

    remove computing lumidata at postprocessing and force to compute later

--- processor/processor.py
+++ processor/processor.py
@@ -669,17 +669,18 @@ class OHProcessor(processor.ProcessorABC):
         
     def postprocess(self, accumulator):
         # compute integrated luminosity
-        if not self.is_data:
-            return accumulator
-        if self.compute_processed_lumi:
-            lumidata = LumiData(self.lumi_csv_path)
-            for dataset in accumulator["processed_lumi"]:
-                accumulator["processed_lumi"][dataset]["lumi_list"].unique() # apply unique
-                accumulator["processed_lumi"][dataset]["lumi"] = lumidata.get_lumi(accumulator["processed_lumi"][dataset]["lumi_list"])
-        else:
-            for dataset in accumulator["processed_lumi"]:
-                accumulator["processed_lumi"][dataset]["lumi_list"].unique() # apply unique
-                accumulator["processed_lumi"][dataset]["lumi"] = None
+        # lumidata might fail, so it is recommended to compute integrated lumi later
+#         if not self.is_data:
+#             return accumulator
+#         if self.compute_processed_lumi:
+#             lumidata = LumiData(self.lumi_csv_path)
+#             for dataset in accumulator["processed_lumi"]:
+#                 accumulator["processed_lumi"][dataset]["lumi_list"].unique() # apply unique
+#                 accumulator["processed_lumi"][dataset]["lumi"] = lumidata.get_lumi(accumulator["processed_lumi"][dataset]["lumi_list"])
+#         else:
+#             for dataset in accumulator["processed_lumi"]:
+#                 accumulator["processed_lumi"][dataset]["lumi_list"].unique() # apply unique
+#                 accumulator["processed_lumi"][dataset]["lumi"] = None
             
 #         if self.lumi_csv_path:
 #             lumidata = LumiData(self.lumi_csv_path)
