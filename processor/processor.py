71501f291e0ce7036e5ebeb5d033ed92d027489a
patinkaew <pinkaew@dx1-915-hep.hip.helsinki.fi>
2023-03-08 14:26:13 +0200

    add lumidata

--- processor/processor.py
+++ processor/processor.py
@@ -260,9 +260,9 @@ class OHProcessor(processor.ProcessorABC):
         cutflow["all events"] += len(events)
         
         # apply lumimask
-        if self.lumimask:
-            events = events[self.lumimask(events.run, events.luminosityBlock)]
-            cutflow["lumimask"] += len(events)
+        #if self.lumimask:
+        #    events = events[self.lumimask(events.run, events.luminosityBlock)]
+        #    cutflow["lumimask"] += len(events)
         # save processed lumi list
         lumi_list = list(set(zip(events.run, events.luminosityBlock)))
         if self.is_data and self.save_processed_lumi:
@@ -668,18 +668,18 @@ class OHProcessor(processor.ProcessorABC):
         return out
         
     def postprocess(self, accumulator):
-#         # compute integrated luminosity
-#         if not self.is_data:
-#             return accumulator
-#         if self.compute_processed_lumi:
-#             lumidata = LumiData(self.lumi_csv_path)
-#             for dataset in accumulator["processed_lumi"]:
-#                 accumulator["processed_lumi"][dataset]["lumi_list"].unique() # apply unique
-#                 accumulator["processed_lumi"][dataset]["lumi"] = lumidata.get_lumi(accumulator["processed_lumi"][dataset]["lumi_list"])
-#         else:
-#             for dataset in accumulator["processed_lumi"]:
-#                 accumulator["processed_lumi"][dataset]["lumi_list"].unique() # apply unique
-#                 accumulator["processed_lumi"][dataset]["lumi"] = None
+        # compute integrated luminosity
+        if not self.is_data:
+            return accumulator
+        if self.compute_processed_lumi:
+            lumidata = LumiData(self.lumi_csv_path)
+            for dataset in accumulator["processed_lumi"]:
+                accumulator["processed_lumi"][dataset]["lumi_list"].unique() # apply unique
+                accumulator["processed_lumi"][dataset]["lumi"] = lumidata.get_lumi(accumulator["processed_lumi"][dataset]["lumi_list"])
+        else:
+            for dataset in accumulator["processed_lumi"]:
+                accumulator["processed_lumi"][dataset]["lumi_list"].unique() # apply unique
+                accumulator["processed_lumi"][dataset]["lumi"] = None
             
 #         if self.lumi_csv_path:
 #             lumidata = LumiData(self.lumi_csv_path)
